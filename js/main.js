0df91a22ff51e93e677c19652be76f056047498c
Rimantas <belauzas@yahoo.com>
2020-07-15 20:23:43 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Kokių konceptų reikės mokytis?
+
+// 1. Kintamieji: gimimo data, šiandienos data, amžius
+// 2. Palyginimo sąlyga: if, switch, ternary operator
+// 3. Ciklai: for, while
+// 4. Funkcijos
+// 5. Event listeners: (reagavimas į) klavietūros paspaudimas, pelės ratukas ir t.t.
