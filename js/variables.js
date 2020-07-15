31850f0b041c16f394328b62fff7b2b72b66f584
Rimantas <belauzas@yahoo.com>
2020-07-15 21:09:03 +0300

    Variables

---
+++ js/variables.js
@@ -0,0 +1,49 @@
+// Kintamųjų tipai:
+// tekstas (string)
+// skaičius (number)
+// loginis (boolean)
+// sąrašas (array)
+// objektas (object)
+
+const name = 'Vardenis';
+const surname = "Pavardenis";
+const name2 = 'Vardenis2';
+const surname2 = "Pavardenis2";
+
+const myAgeIsHere = 99;
+const myAgeInTextFormat = '99';
+
+console.log( 5 );
+console.log( name );
+console.log( surname );
+console.log( name2 );
+console.log( surname2 );
+console.log( myAgeIsHere );
+console.log( myAgeInTextFormat );
+
+let money = 0;
+console.log( 'Šiuo metu turiu pinigų:', money );
+
+money = 1;
+console.log( 'Šiuo metu turiu pinigų:', money );
+
+money = 77;
+console.log( 'Šiuo metu turiu pinigų:', money );
+
+
+const marks = [10, 8, 5, 2, 1];
+console.log( marks );
+
+console.log( 'Jonuko pirmas pažymys buvo:', marks[0] );
+console.log( 'Jonuko antras pažymys buvo:', marks[1] );
+console.log( 'Jonuko paskutinis pažymys buvo:', marks[4] );
+
+const PI = 3.14152965;
+const E = 2.717;
+
+const cities = [ 'Kurėnai, LT', 'Vilnius, LT', 'London, UK', 'Horsens, DK', 'Šilutė, LT', 'Palanga, LT' ];
+console.log( cities );
+
+console.log( 'Aleksandras gyvena:', cities[0] );
+console.log( 'Rimantas gyvena:', cities[1] );
+console.log( 'Alina gyvena:', cities[2] );
